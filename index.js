46831d50370cc85e9eb036f6b2e99bed532dcf58
Myles Borins <myles@famo.us>
2015-06-13 11:43:05 -0700

    Brokenish

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ var path = require('path');
 // npm modules
 var capture = require('webcam-capture');
 var pictureTube = require('picture-tube');
+var eos = require('end-of-stream');
+var through = require('through2');
 
-var camera = capture({});
-var tube = pictureTube();
 
-camera.stdout.pipe(tube).pipe(process.stdout);
+
+function doit() {
+  var camera = capture({});
+  var tube = pictureTube();
+  tube.pipe(through(function (data, enc, cb) {
+    process.stdout.write(data, cb);
+  }));
+
+  camera.stdout.pipe(tube);
+
+  eos(camera, function (err) {
+
+    if (err) { console.error('stream errorororororored'); }
+    doit();
+  });
+}
+
+doit();
